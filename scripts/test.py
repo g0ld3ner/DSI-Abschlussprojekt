fa23736d10a71e6fc43a1c254964289d61d3c198
g0ld3ner <maik.neuhoff@googlemail.com>
2025-07-09 21:47:41 +0200

    bug fix for docker

--- scripts/test.py
+++ scripts/test.py
@@ -1,2 +1,9 @@
 import sklearn
-print(sklearn.__version__)
+import streamlit
+import folium
+import streamlit_folium
+
+print("scikit-learn version:", sklearn.__version__)
+print("streamlit version:", streamlit.__version__)
+print("folium version:", folium.__version__)
+print("streamlit-folium version:", streamlit_folium.__version__)
